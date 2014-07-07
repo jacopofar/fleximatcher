c9a30fa439fb0b8de5a33d9e440ae8e6786ccfe9
jacopofar <jacopo1.farina@gmail.com>
2014-07-07 17:05:40 +0200

    manages subhandler results

--- src/main/java/it/jacopofar/fleximatcher/annotations/ResultPrintingAnnotationHandler.java
+++ src/main/java/it/jacopofar/fleximatcher/annotations/ResultPrintingAnnotationHandler.java
@@ -1,9 +1,12 @@
 package it.jacopofar.fleximatcher.annotations;
 
-import opennlp.tools.util.Span;
-
 import org.json.JSONObject;
 
+import opennlp.tools.util.Span;
+
+/**
+ * An annotation handler which prints debug information on stdout
+ * */
 public class ResultPrintingAnnotationHandler extends DefaultAnnotationHandler {
 
 	private String text;
@@ -15,6 +18,11 @@ public class ResultPrintingAnnotationHandler extends DefaultAnnotationHandler {
 	public void addAnnotation(Span span,JSONObject json) {
 		super.addAnnotation(span,json);
 		System.out.println("Added an annotation for the tag '"+this.getCurrentMatcher()+ "' at "+span+": to '"+span.getCoveredText(text)+"'"+json);
+	}
+	public void addAnnotationFromSubHandler(Span span, JSONObject attributes) {
+		super.addAnnotationFromSubHandler(span,attributes);
+		System.out.println("Added an annotation from a subhandler the tag '"+this.getCurrentMatcher()+ "' at "+span+": to '"+span.getCoveredText(text)+"'"+attributes);
+
 	}
 	
 	public void setCurrentMatcher(String rule){
