094fdf1ec109772c23f501a8959444a449b32a86
jacopofar <jacopo1.farina@gmail.com>
2014-11-04 09:57:07 +0100

    use streams, annotations for overrided methods

--- src/main/java/it/jacopofar/fleximatcher/tag/TagRule.java
+++ src/main/java/it/jacopofar/fleximatcher/tag/TagRule.java
@@ -7,50 +7,51 @@ import java.util.Set;
 import org.json.JSONObject;
 
 import opennlp.tools.util.Span;
-import it.jacopofar.fleximatcher.FlexiMatcher;
 import it.jacopofar.fleximatcher.annotations.AnnotationHandler;
 import it.jacopofar.fleximatcher.annotations.TextAnnotation;
 import it.jacopofar.fleximatcher.rules.MatchingRule;
 
 public class TagRule extends MatchingRule {
-
-	private TagRuleFactory ruleFactory;
-	private String name;
-
-	public TagRule(FlexiMatcher matcher, String tagName) {
-
-	}
-
-	public TagRule(TagRuleFactory tagRuleFactory, String tagName) {
-		this.ruleFactory=tagRuleFactory;
-		this.name=tagName;
-	}
-
-	@Override
-	public boolean annotate(String text, AnnotationHandler ah) {
-		//System.out.println("--"+ah.getNestingLevel()+" TAG RULE: asked to annotate '"+text+"', I'm the rule "+name);
-		if(ah.getNestingLevel()>ruleFactory.getMaximumNesting())
-			return false;
-		ruleFactory.getTagPatterns(name).forEach(pat->{
-			
-			//System.out.println("--"+ah.getNestingLevel()+" about to try pattern: "+pat);
-			AnnotationHandler sa = ah.getSubHandler(pat.getPattern());
-			Optional<Set<LinkedList<TextAnnotation>>> subMatches = ruleFactory.getMatcher().matches(text, pat.getPattern(), sa, false, false,true).getAnnotations();
-			if(subMatches.isPresent()){
-				for(LinkedList<TextAnnotation> matchSequence:subMatches.get()){
-					JSONObject annotation=pat.getResultingAnnotation(text,matchSequence);
-					//System.out.println("--+"+ah.getNestingLevel()+" that pattern ("+pat+") matches with the sequence: "+matchSequence+(annotation==null?"":" annotation: "+annotation.toString()));
-					//there's a match, let's annotate it
-					ah.addAnnotation(new Span(matchSequence.getFirst().getSpan().getStart(),matchSequence.getLast().getSpan().getEnd()),annotation );
-				}
-			}
-		});
-		return ah.checkAnnotationSequence(new String[]{"[tag:"+name+"]"}, text.length(), true,false).isMatching();
-		//now all depending rules were annotated
-	}
-
-	public String toString(){
-		return "tag "+this.name;
-	}
-
+    
+    private final TagRuleFactory ruleFactory;
+    private final String name;
+    
+    public TagRule(TagRuleFactory tagRuleFactory, String tagName) {
+        this.ruleFactory=tagRuleFactory;
+        this.name=tagName;
+    }
+    
+    @Override
+    public boolean annotate(String text, AnnotationHandler ah) {
+        //System.out.println("--"+ah.getNestingLevel()+" TAG RULE: asked to annotate '"+text+"', I'm the rule "+name);
+        if(ah.getNestingLevel()>ruleFactory.getMaximumNesting()){
+            if(ruleFactory.throwExceptionWhenReachingMaximumDepth())
+                throw new RuntimeException("Reached maximum tag expansion depth with "+ah.getNestingLevel()+" for tag '"+name+"'. Check the tag patterns, increase the maximum nestign level, or set the matcher to not throw this exception");
+            else
+                return false;
+        }
+        
+        ruleFactory.getTagPatterns(name).forEach(pat->{
+         
+            //System.out.println("--"+ah.getNestingLevel()+" about to try pattern: "+pat);
+            AnnotationHandler sa = ah.getSubHandler(pat.getPattern());
+            Optional<Set<LinkedList<TextAnnotation>>> subMatches = ruleFactory.getMatcher().matches(text, pat.getPattern(), sa, false, false,true).getAnnotations();
+            if(subMatches.isPresent()){
+                subMatches.get().stream().forEach((matchSequence) -> {
+                    JSONObject annotation=pat.getResultingAnnotation(text,matchSequence);
+                    //System.out.println("--+"+ah.getNestingLevel()+" that pattern ("+pat+") matches with the sequence: "+matchSequence+(annotation==null?"":" annotation: "+annotation.toString()));
+                    //there's a match, let's annotate it
+                    ah.addAnnotation(new Span(matchSequence.getFirst().getSpan().getStart(),matchSequence.getLast().getSpan().getEnd()),annotation );
+                });
+            }
+        });
+        return ah.checkAnnotationSequence(new String[]{"[tag:"+name+"]"}, text.length(), true,false).isMatching();
+        //now all depending rules were annotated
+    }
+    
+    @Override
+    public String toString(){
+        return "tag "+this.name;
+    }
+    
 }
